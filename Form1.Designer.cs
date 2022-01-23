9e0fbfb342e44925084fcdb99c158dcf84e28dd3
20170766 <dmz0128@naver.com>
2022-01-23 20:43:55 +0900

    디자인 수정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,63 +50,63 @@
             // 
             // cmbYear
             // 
-            this.cmbYear.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cmbYear.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.cmbYear.FormattingEnabled = true;
-            this.cmbYear.Location = new System.Drawing.Point(14, 48);
-            this.cmbYear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbYear.Location = new System.Drawing.Point(14, 43);
+            this.cmbYear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cmbYear.Name = "cmbYear";
-            this.cmbYear.Size = new System.Drawing.Size(166, 31);
+            this.cmbYear.Size = new System.Drawing.Size(166, 29);
             this.cmbYear.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(186, 40);
+            this.label1.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(186, 36);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 42);
+            this.label1.Size = new System.Drawing.Size(32, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "년";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // cmbMonth
             // 
-            this.cmbMonth.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cmbMonth.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.cmbMonth.FormattingEnabled = true;
-            this.cmbMonth.Location = new System.Drawing.Point(241, 48);
-            this.cmbMonth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbMonth.Location = new System.Drawing.Point(241, 43);
+            this.cmbMonth.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cmbMonth.Name = "cmbMonth";
-            this.cmbMonth.Size = new System.Drawing.Size(166, 31);
+            this.cmbMonth.Size = new System.Drawing.Size(166, 29);
             this.cmbMonth.TabIndex = 0;
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(414, 38);
+            this.label2.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(414, 34);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 42);
+            this.label2.Size = new System.Drawing.Size(33, 38);
             this.label2.TabIndex = 1;
             this.label2.Text = "월";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // cmbDay
             // 
-            this.cmbDay.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cmbDay.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.cmbDay.FormattingEnabled = true;
-            this.cmbDay.Location = new System.Drawing.Point(473, 48);
-            this.cmbDay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbDay.Location = new System.Drawing.Point(473, 43);
+            this.cmbDay.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cmbDay.Name = "cmbDay";
-            this.cmbDay.Size = new System.Drawing.Size(166, 31);
+            this.cmbDay.Size = new System.Drawing.Size(166, 29);
             this.cmbDay.TabIndex = 0;
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(646, 40);
+            this.label3.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(646, 36);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 42);
+            this.label3.Size = new System.Drawing.Size(33, 38);
             this.label3.TabIndex = 1;
             this.label3.Text = "일";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -114,18 +114,19 @@
             // btnAdd
             // 
             this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAdd.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnAdd.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnAdd.FlatAppearance.BorderSize = 0;
             this.btnAdd.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
             this.btnAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btnAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAdd.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnAdd.Location = new System.Drawing.Point(715, 95);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(714, 85);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(128, 55);
+            this.btnAdd.Size = new System.Drawing.Size(128, 49);
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = false;
@@ -133,18 +134,19 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.Ivory;
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cmbDay);
             this.groupBox1.Controls.Add(this.cmbMonth);
             this.groupBox1.Controls.Add(this.cmbYear);
-            this.groupBox1.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.groupBox1.Location = new System.Drawing.Point(15, 30);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(14, 27);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(683, 115);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(683, 103);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = " 날 짜 ";
@@ -154,32 +156,32 @@
             this.txtPrayerInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPrayerInput.Font = new System.Drawing.Font("나눔고딕", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtPrayerInput.Location = new System.Drawing.Point(16, 197);
-            this.txtPrayerInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPrayerInput.Font = new System.Drawing.Font("배달의민족 주아", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPrayerInput.Location = new System.Drawing.Point(16, 177);
+            this.txtPrayerInput.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtPrayerInput.Name = "txtPrayerInput";
-            this.txtPrayerInput.Size = new System.Drawing.Size(964, 28);
+            this.txtPrayerInput.Size = new System.Drawing.Size(964, 27);
             this.txtPrayerInput.TabIndex = 2;
             this.txtPrayerInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPrayerInput_KeyDown);
             // 
             // label5
             // 
-            this.label5.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(16, 150);
+            this.label5.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(10, 135);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(178, 43);
+            this.label5.Size = new System.Drawing.Size(178, 39);
             this.label5.TabIndex = 5;
             this.label5.Text = "기도제목 입력";
             this.label5.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(16, 230);
+            this.label4.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(10, 208);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(178, 43);
+            this.label4.Size = new System.Drawing.Size(178, 39);
             this.label4.TabIndex = 5;
             this.label4.Text = "기도제목 목록";
             this.label4.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -187,18 +189,19 @@
             // btnRemove
             // 
             this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRemove.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnRemove.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.btnRemove.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnRemove.FlatAppearance.BorderSize = 0;
             this.btnRemove.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
             this.btnRemove.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btnRemove.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
             this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRemove.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnRemove.Location = new System.Drawing.Point(851, 95);
-            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRemove.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRemove.ForeColor = System.Drawing.Color.White;
+            this.btnRemove.Location = new System.Drawing.Point(851, 85);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(128, 55);
+            this.btnRemove.Size = new System.Drawing.Size(128, 49);
             this.btnRemove.TabIndex = 7;
             this.btnRemove.Text = "삭제";
             this.btnRemove.UseVisualStyleBackColor = false;
@@ -207,16 +210,16 @@
             // btnFileControl
             // 
             this.btnFileControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFileControl.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnFileControl.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.btnFileControl.FlatAppearance.BorderSize = 0;
             this.btnFileControl.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
             this.btnFileControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFileControl.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnFileControl.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnFileControl.Location = new System.Drawing.Point(715, 30);
-            this.btnFileControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFileControl.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnFileControl.ForeColor = System.Drawing.Color.White;
+            this.btnFileControl.Location = new System.Drawing.Point(714, 27);
+            this.btnFileControl.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnFileControl.Name = "btnFileControl";
-            this.btnFileControl.Size = new System.Drawing.Size(264, 55);
+            this.btnFileControl.Size = new System.Drawing.Size(264, 49);
             this.btnFileControl.TabIndex = 3;
             this.btnFileControl.Text = "저장 및 불러오기";
             this.btnFileControl.UseVisualStyleBackColor = false;
@@ -227,19 +230,21 @@
             this.lstPrayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstPrayer.BackColor = System.Drawing.Color.Honeydew;
+            this.lstPrayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lstPrayer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.colNum,
             this.colContent,
             this.colDate});
-            this.lstPrayer.Font = new System.Drawing.Font("나눔고딕", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lstPrayer.Font = new System.Drawing.Font("배달의민족 주아", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.lstPrayer.FullRowSelect = true;
             this.lstPrayer.GridLines = true;
             this.lstPrayer.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lstPrayer.HideSelection = false;
-            this.lstPrayer.Location = new System.Drawing.Point(15, 278);
-            this.lstPrayer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstPrayer.Location = new System.Drawing.Point(14, 250);
+            this.lstPrayer.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.lstPrayer.Name = "lstPrayer";
-            this.lstPrayer.Size = new System.Drawing.Size(968, 391);
+            this.lstPrayer.Size = new System.Drawing.Size(968, 352);
             this.lstPrayer.TabIndex = 10;
             this.lstPrayer.UseCompatibleStateImageBehavior = false;
             this.lstPrayer.View = System.Windows.Forms.View.Details;
@@ -247,7 +252,7 @@
             // colNum
             // 
             this.colNum.Text = "번호";
-            this.colNum.Width = 57;
+            this.colNum.Width = 72;
             // 
             // colContent
             // 
@@ -263,10 +268,10 @@
             // 
             // PrayerNote
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(995, 685);
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(994, 615);
             this.Controls.Add(this.lstPrayer);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.groupBox1);
@@ -275,9 +280,9 @@
             this.Controls.Add(this.btnFileControl);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtPrayerInput);
-            this.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Font = new System.Drawing.Font("배달의민족 주아", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Location = new System.Drawing.Point(-10, -10);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "PrayerNote";
             this.Text = "기도노트";
             this.Load += new System.EventHandler(this.PrayerNote_Load);
